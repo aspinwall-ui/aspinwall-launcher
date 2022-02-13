df278f52e00e3c6675f8591a23b3041e95b61fb7
knuxify <knuxify@gmail.com>
2022-02-13 19:43:42 +0100

    tests: add basic tests

---
+++ tests/test_launcher.py
@@ -0,0 +1,51 @@
+# coding: utf-8
+"""
+Basic tests for the launcher
+"""
+import time
+import os
+import pytest
+from gi import require_version as gi_require_version
+gi_require_version("Gtk", "4.0")
+gi_require_version('Adw', '1')
+from gi.repository import Adw, Gtk, Gdk, Gio # noqa: F401
+from gi.repository.Adw import Flap, ToastOverlay, ButtonContent # noqa: F401
+
+@pytest.fixture
+def window():
+	"""Creates the launcher window for use in later tests"""
+	resource = Gio.Resource.load(
+		os.path.join(os.getenv('PWD'), 'output', 'data', 'aspinwall.gresource')
+	)
+	resource._register()
+
+	launcher_resource = Gio.Resource.load(
+		os.path.join(os.getenv('PWD'), 'output', 'src', 'launcher', 'aspinwall.launcher.gresource')
+	)
+	launcher_resource._register()
+
+	import aspinwall.launcher.window
+
+	app = Adw.Application(application_id='org.dithernet.aspinwall.Launcher')
+	app.connect('activate', aspinwall.launcher.window.on_activate)
+
+	aspinwall.launcher.window.on_activate(app)
+
+	assert aspinwall.launcher.window.win is not None
+	return aspinwall.launcher.window.win
+
+def test_app_chooser(window):
+	"""Tests the app chooser revealer."""
+	assert window.launcher_flap
+
+	window.show_app_chooser()
+	time.sleep(0.1)
+	window.launcher_flap.hide()
+
+def test_widget_chooser(window):
+	"""Tests the widget chooser."""
+	assert window.widgetbox.widget_chooser
+
+	window.widgetbox.show_chooser()
+	time.sleep(0.1)
+	window.widgetbox.widget_chooser.hide()
---
+++ tests/test_widgets.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+"""
+Contains tests for the widget API.
+"""
+import gi
+gi.require_version('Gtk', '4.0')
+from gi.repository import Gtk
+from uuid import uuid4
+
+from aspinwall.widgets import Widget
+
+class ExampleWidget(Widget):
+	"""Example widget class used for tests."""
+	metadata = {
+		"id": "org.dithernet.aspinwall.widgets.testwidget",
+		"name": "Test widget",
+		"icon": 'dialog-information-symbolic',
+		"description": "This is a widget used for tests.",
+		"tags": ['test', 'test widget', 'example']
+	}
+
+	def __init__(self, instance):
+		super().__init__(instance)
+
+def test_widget():
+	"""Creates a widget and tests its basic functions."""
+	widget = ExampleWidget(instance=uuid4)
+	assert widget
+	assert widget.instance
+
+	# Make sure style provider is created correctly
+	assert type(widget.css_provider) == Gtk.CssProvider
+
+	# By default, widgets that don't set their own refresh function have an
+	# empty placeholder function that always returns None:
+	assert widget.refresh() is None
+
+	# Test properties
+	assert widget.id == widget.metadata['id']
+	assert widget.name == widget.metadata['name']
+	assert widget.icon_name == widget.metadata['icon']
+	assert widget.description == widget.metadata['description']
+	assert widget.tags == widget.metadata['tags']
