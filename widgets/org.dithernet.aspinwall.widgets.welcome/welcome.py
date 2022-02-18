cee00de4e30a2807341a974c1fa4581d9dc01366
knuxify <knuxify@gmail.com>
2022-02-18 20:32:07 +0100

    widgets: welcome: add more information

--- widgets/org.dithernet.aspinwall.widgets.welcome/welcome.py
+++ widgets/org.dithernet.aspinwall.widgets.welcome/welcome.py
@@ -3,7 +3,9 @@
 Welcome plugin for Aspinwall
 """
 from aspinwall.widgets import Widget
-from gi.repository import Gtk
+from gi.repository import Gtk, Adw
+
+from gettext import gettext as _
 
 class Welcome(Widget):
 	metadata = {
@@ -17,11 +19,40 @@ class Welcome(Widget):
 	def __init__(self, instance=0):
 		super().__init__(instance)
 
-		container = Gtk.Box(halign=Gtk.Align.FILL, hexpand=True)
-		container.append(Gtk.Label(label='To add new widgets, press the '))
-		container.append(Gtk.Image.new_from_icon_name('open-menu-symbolic'))
-		container.append(Gtk.Label(label=' button in the top right corner.'))
+		header = Gtk.Label(
+			label=_('Welcome to Aspinwall'),
+			use_markup=True, hexpand=True,
+			margin_top=10, margin_bottom=10
+		)
+		header.add_css_class('large-title')
+		self.content.append(header)
 
-		self.content = container
+		info_clamp = Adw.Clamp(maximum_size=700)
+		info = Gtk.Box(hexpand=True, orientation=Gtk.Orientation.VERTICAL)
+		info_clamp.set_child(info)
+
+		thankyou = Gtk.Label(
+			label=_("Thank you for trying out Aspinwall! The UI is still in active development, and some things may not work correctly - you can report bugs to <a href='https://github.com/aspinwall-ui/aspinwall/issues'>our bug tracker</a>."), # noqa: E501
+			wrap=True, use_markup=True
+		)
+		thankyou.add_css_class('body')
+
+		widgetguide = Gtk.Box(hexpand=True)
+
+		widgetguide = Gtk.Label(
+			label=_("""
+• To add a new widget, press the menu button in the top right corner, then select “Add Widgets”.
+• To remove this widget, long-press it and click the trash icon on the toolbar that pops up.
+"""), # noqa: E501
+			wrap=True
+		)
+
+		widgetguide.add_css_class('body')
+		widgetguide.add_css_class('welcome-text')
+
+		info.append(thankyou)
+		info.append(widgetguide)
+
+		self.content.append(info_clamp)
 
 _widget_class = Welcome
