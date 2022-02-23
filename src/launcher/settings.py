7356f5ab7d9bf170cc5155bc6f9bc909380f085b
knuxify <knuxify@gmail.com>
2022-02-23 20:44:21 +0100

    launcher: settings: implement adding/removing available wallpapers

--- src/launcher/settings.py
+++ src/launcher/settings.py
@@ -32,6 +32,13 @@ class WallpaperIcon(Gtk.FlowBoxChild):
 
 		self.picture.set_pixbuf(pixbuf)
 
+	@Gtk.Template.Callback()
+	def remove_from_available(self, *args):
+		"""Removes the wallpaper from available wallpapers."""
+		new_config = config['available-wallpapers'].copy()
+		new_config.remove(self.wallpaper)
+		config['available-wallpapers'] = new_config
+
 @Gtk.Template(resource_path='/org/dithernet/aspinwall/launcher/ui/settings.ui')
 class LauncherSettings(Adw.PreferencesWindow):
 	"""Launcher settings window."""
@@ -61,15 +68,14 @@ class LauncherSettings(Adw.PreferencesWindow):
 			self.theme_toggle_start.set_active(True)
 			self.follow_system_theme_toggle.set_active(True)
 
-		wallpaper_store = Gtk.StringList()
-		wallpaper_store.splice(0, 0, config['available-wallpapers'])
-
+		self.wallpaper_store = Gtk.StringList()
+		self.wallpaper_store.splice(0, 0, config['available-wallpapers'])
 		self.wallpaper_grid.bind_model(
-			wallpaper_store,
+			self.wallpaper_store,
 			self.wallpaper_bind,
-			None,
-			self.wallpaper_free
+			None
 		)
+		config.connect('changed::available-wallpapers', self.update_wallpaper_grid)
 
 		if bg_config:
 			self.system_wallpaper_settings_toggle.set_active(config['use-gnome-background'])
@@ -85,8 +91,56 @@ class LauncherSettings(Adw.PreferencesWindow):
 		wallpaper_path = wallpaper_path_string.get_string()
 		return WallpaperIcon(wallpaper_path)
 
-	def wallpaper_free(self, *args):
-		print('Free:', args)
+	def update_wallpaper_grid(self, *args):
+		"""Updates the wallpaper grid after a removal/addition."""
+		_current_store = list(self.wallpaper_store)
+		current_store = []
+		# This contains StringObjects, so convert them to strings first
+		for item in _current_store:
+			current_store.append(item.get_string())
+		current_config = config['available-wallpapers'].copy()
+
+		new_wallpapers = list(set(current_store) - set(current_config)) + \
+			list(set(current_config) - set(current_store))
+
+		for wallpaper in new_wallpapers:
+			if wallpaper in current_config:
+				# Wallpaper added
+				self.wallpaper_store.append(wallpaper)
+			else:
+				# Wallpaper removed
+				self.wallpaper_store.remove(current_store.index(wallpaper))
+
+	@Gtk.Template.Callback()
+	def show_wallpaper_add_dialog(self, *args):
+		"""Shows the wallpaper addition dialog."""
+		file_dialog = Gtk.FileChooserDialog(
+			title=_('Add wallpaper'),
+			action=Gtk.FileChooserAction.OPEN,
+		)
+
+		file_dialog.add_buttons(
+			# TRANSLATORS: Used for the file chooser dialog for adding wallpapers
+			_('_Cancel'), Gtk.ResponseType.CANCEL,
+			# TRANSLATORS: Used for the file chooser dialog for adding wallpapers
+			_('_Select'), Gtk.ResponseType.OK
+		)
+
+		file_dialog.set_transient_for(self)
+		file_dialog.show()
+
+		file_dialog.connect('response', self.add_wallpaper)
+
+	def add_wallpaper(self, dialog, response):
+		"""Callback for the wallpaper addition dialog."""
+		if response == Gtk.ResponseType.OK:
+			file = dialog.get_file()
+			wallpaper_path = file.get_path()
+			new_wallpapers = config['available-wallpapers'].copy()
+			new_wallpapers.append(wallpaper_path)
+			config['available-wallpapers'] = new_wallpapers
+
+		dialog.destroy()
 
 	@Gtk.Template.Callback()
 	def set_wallpaper_from_grid(self, flowbox, wallpaper_icon):
