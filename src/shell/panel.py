2ca4cb207e0cd87bf44157f4ead47cc58095cfc1
knuxify <knuxify@gmail.com>
2022-03-11 21:16:32 +0100

    shell: panel: make clock work

--- src/shell/panel.py
+++ src/shell/panel.py
@@ -5,12 +5,22 @@ Contains the code for the panel.
 from gi.repository import Gtk
 
 from aspinwall.shell.surface import Surface
+from aspinwall.utils.clock import clock_daemon
+import time
 
 @Gtk.Template(resource_path='/org/dithernet/aspinwall/shell/ui/panel.ui')
 class Panel(Surface):
 	"""The status bar on the top of the screen."""
 	__gtype_name__ = 'Panel'
 
+	clock = Gtk.Template.Child()
+
 	def __init__(self, app):
 		"""Initializes the panel."""
 		super().__init__(application=app, hexpand=True, height=32)
+		clock_daemon.connect('notify::time', self.update_time)
+		self.clock.set_label(time.strftime('%H:%M'))
+
+	def update_time(self, *args):
+		"""Updates the time on the clock."""
+		self.clock.set_label(time.strftime('%H:%M'))
